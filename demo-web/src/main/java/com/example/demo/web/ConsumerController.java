0cfae51e145352b12a77e7a3be0ad67a5eda5612
Ryan Su <su.shiran@163.com>
2021-02-20 19:01:16 +0800

    demo1 引入负载 LoadBalance接口 Ribbon实现 写法1

--- demo-web/src/main/java/com/example/demo/web/ConsumerController.java
+++ demo-web/src/main/java/com/example/demo/web/ConsumerController.java
@@ -2,33 +2,28 @@ package com.example.demo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/c")
 public class ConsumerController {
 
+    @Autowired
     private RestTemplate restTemplate;
 
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient lb;
 
     @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
     public String echo(@PathVariable String str) {
-        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("demo");
-        System.out.println(serviceInstances.get(0).getUri().toString());
-        return restTemplate.getForObject(serviceInstances.get(0).getUri().toString() + "/p/echo/" + str, String.class);
+        ServiceInstance serviceInstance = lb.choose("demo");
+        System.out.println(serviceInstance.getUri().toString());
+        return restTemplate.getForObject(serviceInstance.getUri().toString() + "/p/echo/" + str, String.class);
     }
 
-    @Autowired
-    public void setRestTemplate(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 }
