874c33f46ee6095faa0ec892aeac713c6968f886
Ryan Su <su.shiran@163.com>
2021-02-20 19:04:05 +0800

    demo1 引入负载 LoadBalance接口 Ribbon实现 写法2

--- demo-web/src/main/java/com/example/demo/web/ConsumerController.java
+++ demo-web/src/main/java/com/example/demo/web/ConsumerController.java
@@ -1,8 +1,6 @@
 package com.example.demo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,14 +14,9 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private LoadBalancerClient lb;
-
     @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
     public String echo(@PathVariable String str) {
-        ServiceInstance serviceInstance = lb.choose("demo");
-        System.out.println(serviceInstance.getUri().toString());
-        return restTemplate.getForObject(serviceInstance.getUri().toString() + "/p/echo/" + str, String.class);
+        return restTemplate.getForObject("http://demo/p/echo/" + str, String.class);
     }
 
 }
